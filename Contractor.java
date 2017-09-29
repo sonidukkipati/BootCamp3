879edfdf28fd5f37352b57d6e4dee35be134515f
sonidukkipati <sonichalasani@yahoo.com>
2017-09-29 10:49:56 -0700

    Update Contractor.java

--- Contractor.java
+++ Contractor.java
@@ -3,6 +3,8 @@ package Level3Homework6;
 /**
  * Created by sc on 5/8/2017.
  */
+
+// He is the Contractor : another Employee type
 public class Contractor extends Employee {
 
     public Contractor(){
